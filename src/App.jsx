100e6dbe40965a3aad7ba06b0202bd098dd3e33a
Sebastián Nuñez <sebas11np@gmail.com>
2025-01-25 16:58:37 -0600

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 
 export const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [products, setProducts] = useState([]);
+
+  const datos = async () => {
+    const url =
+      "https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/4-frontend-libaries/challenges/group_1/data/simple-coffee-listing-data.json";
+    const datos = await fetch(url);
+    const data = await datos.json();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    datos();
+  }, []);
+
+  return <div>App</div>;
+};
